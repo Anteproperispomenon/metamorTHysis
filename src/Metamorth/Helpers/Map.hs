7383e5a390a4458a263659543966cfa62f7d5b39
David Wilson <30732161+Anteproperispomenon@users.noreply.github.com>
2024-02-23 14:16:42 -0500

    Added various code.
    Added aspects to the trait lookup types (so the parser can get know the aspects of the character it just parsed).
    Also added a bunch of code for generating declaration for phoneme types, but still need to rig up some more code for looking up information about phonemes.

--- src/Metamorth/Helpers/Map.hs
+++ src/Metamorth/Helpers/Map.hs
@@ -1,6 +1,7 @@
 module Metamorth.Helpers.Map
   ( forWithKey
   , fromSelfList
+  , forMapWithKey
   ) where
 
 import Data.Map.Strict qualified as M
@@ -13,9 +14,16 @@ import Data.Map.Strict qualified as M
 -- 
 forWithKey :: Applicative t => M.Map k a -> (k -> a -> t b) -> t (M.Map k b)
 forWithKey mp f = M.traverseWithKey f mp
+{-# INLINE forWithKey #-}
 
 -- | Create a `M.Map` from a simple list,
 --   where every element is used as both
 --   the key and the value.
 fromSelfList :: Ord a => [a] -> M.Map a a
 fromSelfList = M.fromList . (map (\x -> (x,x)))
+
+-- | Like `M.MapWithKey`, but with the arguments
+--   reversed.
+forMapWithKey :: M.Map k a -> (k -> a -> b) -> M.Map k b
+forMapWithKey mp f = M.mapWithKey f mp
+{-# INLINE forMapWithKey #-}
