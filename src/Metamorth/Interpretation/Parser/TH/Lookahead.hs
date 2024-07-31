2da142909c988cfcaa71401942f83d047911a61f
David Wilson <30732161+Anteproperispomenon@users.noreply.github.com>
2024-07-31 13:18:47 -0400

    Fixed issue with lookahead and states.
    Now can handle patterns where not all lookahead branches have the same state modifications.

--- src/Metamorth/Interpretation/Parser/TH/Lookahead.hs
+++ src/Metamorth/Interpretation/Parser/TH/Lookahead.hs
@@ -3,7 +3,11 @@
 module Metamorth.Interpretation.Parser.TH.Lookahead
   ( createLookahead
   , createMultiLookahead
+  , createMultiLookahead2
   ) where
+
+import Control.Applicative
+import Control.Monad
 
 import Data.Attoparsec.Text qualified as AT
 
@@ -15,8 +19,11 @@ import Metamorth.Helpers.Parsing
 import Metamorth.Interpretation.Parser.Types
 
 import Data.List.NonEmpty qualified as NE
+import Data.List.NonEmpty (NonEmpty(..))
+
 import Metamorth.Helpers.TH
 
+import Control.Monad.Trans.State.Strict qualified as St
 
 createLookahead :: (Quote q) => {-[ModifyStateX]-} Exp -> Name -> (Exp -> Exp) -> Exp -> q Exp
 createLookahead stModLamb funcName rsltCheck rslt = do
@@ -53,7 +60,9 @@ createMultiLookahead stModLamb funcName rsltChecks otherRslt = do
   where
     otherRslt'
       | (Just oRslt) <- otherRslt
-      = AppE (VarE 'return) oRslt
+      = if (stModLamb == (VarE 'id))
+        then AppE (VarE 'return) oRslt
+        else InfixE (Just (AppE (VarE 'St.modify') stModLamb)) (VarE '(>>)) (Just (AppE (VarE 'return) oRslt))
       -- = [| return $(pure oRslt) |]
       | otherwise
       =  AppE (VarE 'fail) (LitE (StringL "Could not find a lookahead."))
@@ -66,10 +75,32 @@ createMultiLookahead stModLamb funcName rsltChecks otherRslt = do
     ifBlocks :: Name -> [(Guard, Exp)]
     ifBlocks nom = ifBlocks' nom ++ [(NormalG $ VarE 'otherwise, otherRslt')]
     
-
+-- Warning: Needs to modify state!
+-- Also: What if there are multiple lookaheads that
+-- make different moddifications to the state(s)?
 
 -- MultiIfE [(Guard, Exp)]
 -- data Guard = NormalG Exp | PatG [Stmt]
+
+-- | For use when you have multiple possible
+--   lookaheads that have different state modifications.
+createMultiLookahead2 :: (Quote q) => Name -> [(Exp, [(Exp -> Exp, Exp)])] -> Maybe (Exp, Exp) -> q Exp
+createMultiLookahead2 funcName modCases otherRslt = do
+  rsltList <- forM modCases $ \(stModLamb, rsltChecks) -> 
+    createMultiLookahead stModLamb funcName rsltChecks Nothing
+  return $ intersperseInfixRE (VarE '(<|>)) (snocNE rsltList otherRslt')
+  where
+    otherRslt'
+      | Just (oRslt, stMod) <- otherRslt
+      = if (stMod == (VarE 'id))
+        then AppE (VarE 'return) oRslt
+        else InfixE (Just (AppE (VarE 'St.modify') stMod)) (VarE '(>>)) (Just (AppE (VarE 'return) oRslt))
+      | otherwise
+      =  AppE (VarE 'fail) (LitE (StringL "Could not find a lookahead."))
+
+snocNE :: [a] -> a -> NonEmpty a
+snocNE []     y = y :| []
+snocNE (x:xs) y = x :| (xs ++ [y])
 
 {-
 lookAheadSX :: (s -> s) -> (State.StateT s AT.Parser a) -> (State.StateT s AT.Parser a)
@@ -77,3 +108,6 @@ lookAheadSX f prs = do
   st <- State.get
   lift $ AC.lookAhead (State.evalStateT prs (f $! st))
 -}
+
+-- intersperseInfixRE (VarE '(<|>))
+
